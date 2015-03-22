b25d682cfbc6fcf5f15a52306cd8b4ad38088fa3
kkayed <kkayed@hsr.ch>
2015-03-22 10:35:09 +0100

    - New API for messaging via Background Service - OSMAND can send messages to the watch app now!!

--- OsmAnd/src/net/osmand/plus/OsmandPlugin.java
+++ OsmAnd/src/net/osmand/plus/OsmandPlugin.java
@@ -23,6 +23,7 @@ import net.osmand.plus.osmo.OsMoPlugin;
 import net.osmand.plus.parkingpoint.ParkingPositionPlugin;
 import net.osmand.plus.rastermaps.OsmandRasterMapsPlugin;
 import net.osmand.plus.skimapsplugin.SkiMapsPlugin;
+import net.osmand.plus.smartnaviwatch.SmartNaviWatchPlugin;
 import net.osmand.plus.srtmplugin.SRTMPlugin;
 import net.osmand.plus.views.OsmandMapTileView;
 
@@ -111,7 +112,8 @@ public abstract class OsmandPlugin {
 		allPlugins.add(new AccessibilityPlugin(app));
 		allPlugins.add(new OsmEditingPlugin(app));
 		allPlugins.add(new OsmandDevelopmentPlugin(app));
-		
+        allPlugins.add(new SmartNaviWatchPlugin(app));
+
 		for (OsmandPlugin plugin : allPlugins) {
 			if (enabledPlugins.contains(plugin.getId()) || plugin.isActive()) {
 				try {
---
+++ OsmAnd/src/net/osmand/plus/smartnaviwatch/SmartNaviWatchPlugin.java
@@ -0,0 +1,119 @@
+package net.osmand.plus.smartnaviwatch;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.util.Log;
+
+import net.osmand.Location;
+import net.osmand.plus.OsmAndLocationProvider;
+import net.osmand.plus.OsmandApplication;
+import net.osmand.plus.OsmandPlugin;
+import net.osmand.plus.R;
+import net.osmand.plus.routing.RouteCalculationResult;
+import net.osmand.plus.routing.RouteDirectionInfo;
+import net.osmand.plus.routing.RoutingHelper;
+import net.osmand.util.Algorithms;
+
+import java.util.List;
+
+import ch.hsr.navigationmessagingapi.MessageEndPoint;
+import ch.hsr.navigationmessagingapi.NavigationMessage;
+import ch.hsr.navigationmessagingapi.services.NavigationServiceConnector;
+
+public class SmartNaviWatchPlugin extends OsmandPlugin{
+
+    private OsmandApplication application;
+    private RoutingHelper routing;
+    private RouteCalculationResult.NextDirectionInfo currentInfo;
+    private OsmAndLocationProvider location;
+    private NavigationServiceConnector messageService;
+
+    private NavigationServiceConnector getServiceConnector() {
+        if (messageService == null)
+        {
+            messageService = new NavigationServiceConnector(application.getApplicationContext());
+        }
+        return messageService;
+    }
+
+    private boolean stepsAreDifferent(RouteCalculationResult.NextDirectionInfo oldInfo, RouteCalculationResult.NextDirectionInfo newDirection) {
+        return true;
+    }
+
+    private void updateNavigationSteps() {
+        application.showShortToastMessage("update steps");
+        RouteCalculationResult.NextDirectionInfo n = routing.getNextRouteDirectionInfo(new RouteCalculationResult.NextDirectionInfo(), false);
+
+        if(stepsAreDifferent(currentInfo, n)) {
+            currentInfo = n;
+
+          if (currentInfo != null) application.showShortToastMessage("w: " + currentInfo.directionInfo.getDescriptionRoute(application));
+        }
+    }
+
+    private class RoutingAdapter implements RoutingHelper.IRouteInformationListener {
+        @Override
+        public void newRouteIsCalculated(boolean newRoute) {
+            // clean the old lists
+
+            updateNavigationSteps();
+            NavigationMessage msg = new NavigationMessage();
+            msg.setMessageType("/navigation/route/new");
+            msg.setPayload(new Integer(3));
+            getServiceConnector().sendMessage(msg);
+        }
+
+        @Override
+        public void routeWasCancelled() {
+
+        }
+    }
+
+    private class LocationAdapter implements OsmAndLocationProvider.OsmAndLocationListener{
+        @Override
+        public void updateLocation(Location location) {
+            //updateNavigationSteps();
+        }
+    }
+
+    public SmartNaviWatchPlugin(OsmandApplication app)
+    {
+        application = app;
+    }
+
+    @Override
+    public boolean init(OsmandApplication app, Activity activity) {
+        routing = app.getRoutingHelper();
+        location = app.getLocationProvider();
+
+        routing.addListener(new RoutingAdapter());
+        location.addLocationListener(new LocationAdapter());
+
+        return true;
+    }
+
+    @Override
+    public Class<? extends Activity> getSettingsActivity() {
+        return null;
+    }
+
+    @Override
+    public String getId() {
+        return "ch.hsr.smart-navi-watch";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Sends data to your watch.";
+    }
+
+    @Override
+    public String getName() {
+        return "Smart Navi Watch";
+    }
+
+    @Override
+    public int getAssetResourceName() {
+        return R.drawable.parking_position;
+    }
+}
